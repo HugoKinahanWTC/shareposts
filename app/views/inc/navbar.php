1bcf1fa2f92b82fea43c08a6b01b23c335af486d
HugoKinahan <hugokinahan@hotmail.com>
2022-02-08 14:52:11 +0000

    register and login pages added with forms

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?=
+          <a class="nav-link" aria-current="page" href="<?=
           URLROOT; ?>">Home</a>
         </li>
         <li class="nav-item">
@@ -16,7 +16,7 @@
       </ul>
         <ul class="navbar-nav ml-auto mb-2 mb-md-0">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="<?=
+                <a class="nav-link" aria-current="page" href="<?=
                 URLROOT; ?>/users/register">Register</a>
             </li>
             <li class="nav-item">
